1dd6fbe17133924651e6399b46740380d3bd1982
Dmitry Moskowski <me@corpix.ru>
2018-02-20 19:51:35 +0000

    Refactored nix-cage

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "nix-cage-shell";
   buildInputs = [
-    
     gocode
     glide
     godef
+    python36
+    gnumake
   ];
   shellHook = ''
     export GOPATH=~/projects
