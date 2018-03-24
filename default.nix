cc3ec8f28a2cbc6564320f6c4ce677780e47afb5
Dmitry Moskowski <me@corpix.ru>
2018-03-24 16:25:58 +0000

    Updated derivation

--- default.nix
+++ default.nix
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner  = "corpix";
     repo   = name;
-    rev    = "e6f91a9ef291387a373517e433b6cde4ed851bce";
-    sha256 = "0nwdxvb3an3awlvls610qvdj4hvmfz5xryws49ja9j5yrjv298k0";
+    rev    = "75fd5cf1490022307dbf79d62535095b92a7c081";
+    sha256 = "101lxi0hnwxkc7racs729rcambvm13pgzlfsz59wc9k8jlw9mx1g";
   };
 
   buildPhase = ''
